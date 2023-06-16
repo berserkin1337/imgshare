592cf36147c630843a95196ca834d9a89b2a2bd8
Aviral Gupta <gaviral373@gmail.com>
2023-06-16 16:04:07 +0530

    Initial Commit

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+use axum::{response::IntoResponse, routing::get, Json, Router};
+
+pub async fn health_checker_handler() -> impl IntoResponse {
+    let json_response = serde_json::json!({
+        "status": "success",
+    });
+
+    Json(json_response)
+}
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/api/healthchecker", get(health_checker_handler));
+
+    println!("🚀 Server started successfully");
+    axum::Server::bind(&"0.0.0.0:8000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
